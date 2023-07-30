6c1e6926b058c219b60dbf1de311880f5468d7c7
GholamHackeram <140943446+GholamHackeram@users.noreply.github.com>
2023-07-30 12:32:02 +0330

    Add files via upload

---
+++ Telegram Hack.py
@@ -0,0 +1,45 @@
+# By GholamHackerrrrrrrrrr
+# Hahahahahahaha
+
+from time import sleep
+
+from colorama import Fore as color
+
+print(color.RED+"Welcome To Telegram Hack App")
+
+print(color.BLUE+"Developer(s): GholamHacker")
+
+print(color.BLACK+"Enter Target ID")
+
+id = input(color.GREEN+"--->")
+
+id = id.strip("@")
+
+print(color.CYAN+"Please Wait...")
+
+print(color.GREEN+"Username: ",end="")
+
+sleep(3)
+
+print(color.RED+"Hacked!")
+
+sleep(1)
+
+print(color.GREEN+"Password: ", end="")
+
+sleep(4)
+
+print(color.RED+"Hacked!")
+
+sleep(1)
+
+print(color.GREEN+"Telgram Bypass...")
+
+sleep(4)
+
+print(color.BLUE+"Succecfully!")
+
+sleep(1)
+
+print(color.RED+F"@{id} Hacked!")
+
